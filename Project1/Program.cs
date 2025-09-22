6d754734df49011be57369037957abb94d4c8696
LedyaBakloug <lbakloug@gmail.com>
2025-09-22 18:46:46 -0400

    change to web app and add serilog

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using Serilog;
+
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+            builder.Host.UseSerilog();
+
+            var app = builder.Build();
+            app.MapGet("/", (ILogger<Program> logger) =>
+            {
+                logger.LogInformation("Hello, world!");
+                return "Hello World!";
+            });
+            app.Run();
+        }
+    }
+}
