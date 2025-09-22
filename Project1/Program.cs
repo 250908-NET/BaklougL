66977c09002617ee502026e6d9ee121d0b083e6a
LedyaBakloug <lbakloug@gmail.com>
2025-09-22 19:06:10 -0400

    add swagger

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -13,10 +13,24 @@ namespace Project1
 
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddOpenApi();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
             Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
             builder.Host.UseSerilog();
 
             var app = builder.Build();
+
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+
             app.MapGet("/", (ILogger<Program> logger) =>
             {
                 logger.LogInformation("Hello, world!");
